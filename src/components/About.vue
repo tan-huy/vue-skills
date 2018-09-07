71392943ed02fb45d1c446fd5892165e5cbe4cac
Tan Huy H. Nguyen <tanhuy@me.com>
2018-09-07 19:54:44 +0900

    To-do list style app.

---
+++ src/components/About.vue
@@ -0,0 +1,17 @@
+<template>
+    <div class="about">
+        <h1>Hello {{user_name}}, this is an about page</h1>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Modi incidunt autem quod, beatae itaque minima fuga tempore temporibus atque! Vitae?</p>
+    </div>
+</template>
+
+<script>
+export default {
+  name: 'Skills',
+    data() {
+        return {
+            user_name: this.$route.params.name
+        }
+    }
+}
+</script>
--- src/components/Skills.vue
+++ src/components/Skills.vue
@@ -1,8 +1,21 @@
 <template>
   <div class="hello">
     <div class="holder">
+
+      <form @submit.prevent="addSkill">
+      <input type="text" placeholder="Enter a skill you have.." v-model="skill" v-validate="'min:5'" name="skill">
+      
+      <transition name="alert-in" enter-active-class="animated flash" leave-active-class="animated fadeOut">
+      <p class="alert" v-if="errors.has('skill')">{{errors.first('skill')}}</p>
+      </transition>
+      </form>
+      
       <ul>
-        <li v-for="(data, index) in skills" :key='index'>{{data.skill}}</li>
+        <transition-group name="list" enter-active-class="animated bounceInUp" leave-active-class="animated bounceOutDown">
+        <li v-for="(data, index) in skills" :key='index'>{{data.skill}}
+          <i class="fa fa-minus-circle" v-on:click="remove(index)"></i>
+        </li>
+        </transition-group>
       </ul>
 
       <p>These are the skills that you possess.</p>
@@ -15,13 +28,26 @@ export default {
   name: 'Skills',
   data() {
     return {
+      skill: '',
       skills: [
         { "skill": "Vue.js"},
         { "skill": "Frontend Developer"}
-      ],
-      bgColor: 'yellow',
-      bgWidth: '100%',
-      bgHeight: '30px'
+      ]
+    }
+  },
+  methods: {
+    addSkill() {
+      this.$validator.validateAll().then((result) => {
+        if (result) {
+          this.skills.push({skill: this.skill})
+          this.skill = '';
+        } else {
+            console.log('Not valid');
+          }
+      })
+    },
+    remove(id) {
+      this.skills.splice(id,1);
     }
   }
 }
@@ -29,6 +55,9 @@ export default {
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
+@import "https://cdn.jsdelivr.net/npm/animate.css@3.5.1";
+@import "https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css";
+
 .holder {
     background: #fff;
   }
@@ -57,5 +86,46 @@ export default {
   .container {
     box-shadow: 0px 0px 40px lightgray;
   }
+
+  input {
+    width: calc(100% - 40px);
+    border: 0;
+    padding: 20px;
+    font-size: 1.3em;
+    background-color: #323333;
+    color: #687F7F;
+  }
+
+  .alert {
+    background: #fdf2ce;
+    font-weight: bold;
+    display: inline;
+    padding: 0;
+  }
+
+  .alert-in-enter-active {
+    animation: bounce-in .5s;
+  }
+
+  .alert-in-leave-active {
+    animation: bounce-in .5s reverse;
+  }
+
+@keyframes bounce-in {
+  0% {
+    transform: scale(0);
+  }
+  50% {
+    transform: scale(1.5);
+  }
+  100% {
+    transform: scale(1);
+  }
+}
+
+i {
+  float: right;
+  cursor: pointer;
+}
 
 </style>
